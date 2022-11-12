3c3b0bf030ca50ccd3faa9a9421decafec47c2ff
Thanayos Siratranont <tuaa1bb2@gmail.com>
2022-11-12 21:48:15 +0700

    Change: Park's Ground Texture

---
+++ Assets/Scripts/Tutor's Script/Serializable Class/InterfaceElement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Presentation/Interface Element")]
+public class InterfaceElement : ScriptableObject
+{
+    public Sprite Interact;
+    public Sprite InteractHover;
+    public Sprite ForceInteract;
+    public Sprite ForceInteractHover;
+    
+    public Sprite
+}
