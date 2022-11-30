cb6f8da7c4f240ca481a12873bef961becb7768a
Thanayos Siratranont <tuaa1bb2@gmail.com>
2022-11-30 23:31:23 +0700

    Fix: Appearance now can use without assign gameObject

--- Assets/Scripts/Tutor's Script/Appearance.cs
+++ Assets/Scripts/Tutor's Script/Appearance.cs
@@ -9,6 +9,34 @@ public class Appearance : MonoBehaviour
     [SerializeField] private string eventKey;
     [SerializeField] private bool defaultAppearance;
 
+    private void Awake()
+    {
+        if(controlledGameObject == null) controlledGameObject = gameObject;
+        
+        if (defaultAppearance)
+        {
+            if (controlledGameObject.TryGetComponent(out MeshRenderer meshT))
+            {
+                meshT.enabled = true;
+            }
+            else
+            {
+                controlledGameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            if (controlledGameObject.TryGetComponent(out MeshRenderer meshT))
+            {
+                meshT.enabled = false;
+            }
+            else
+            {
+                controlledGameObject.SetActive(false);
+            }
+        }
+    }
+
     private void FixedUpdate()
     {
         if (EventHorizon.Instance.EventsHorizon[eventKey])
